b689ba89ab8a958af31b4b89d8161a477064a737
Tomasz Janicki <tomaszjanicki@ciber.com>
2016-06-17 20:56:12 +0200

    Add project files.

---
+++ Projects/Architecture3.WebApi/App_Start/RegisterRoutes.cs
@@ -0,0 +1,13 @@
+﻿namespace Architecture3.WebApi
+{
+    using System.Web.Http;
+
+    public static class RegisterRoutes
+    {
+        public static void Execute(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute("DefaultApi", "{controller}/{id}", new { id = RouteParameter.Optional });
+        }
+    }
+}
---
+++ Projects/Architecture3.WebApi/Startup.cs
@@ -0,0 +1,16 @@
+﻿namespace Architecture3.WebApi
+{
+    using System.Web.Http;
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var configuration = new HttpConfiguration();
+            RegisterRoutes.Execute(configuration);
+            appBuilder.UseWebApi(configuration);
+            appBuilder.UseWelcomePage();
+        }
+    }
+}
