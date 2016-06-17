2fcf09593b6ba26dca6732805b61d8df353be322
Tomasz Janicki <tomaszjanicki@ciber.com>
2016-06-17 21:25:58 +0200

    removed not used formatters

--- Projects/Architecture3.WebApi/App_Start/RegisterMiscs.cs
+++ Projects/Architecture3.WebApi/App_Start/RegisterMiscs.cs
@@ -1,11 +1,31 @@
 ﻿namespace Architecture3.WebApi
 {
+    using System.Linq;
+    using System.Net.Http.Formatting;
     using System.Web.Http;
+    using Newtonsoft.Json.Converters;
+    using Newtonsoft.Json.Serialization;
 
     public static class RegisterMiscs
     {
         public static void Execute(HttpConfiguration configuration)
         {
+            configuration.Formatters.Clear();
+            configuration.Formatters.Add(GetConfiguredJsonMediaTypeFormatter());
+        }
+
+        private static JsonMediaTypeFormatter GetConfiguredJsonMediaTypeFormatter()
+        {
+            var result = new JsonMediaTypeFormatter();
+            var mediaTypeHeaderValue = result.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "text/json");
+            if (mediaTypeHeaderValue != null)
+            {
+                result.SupportedMediaTypes.Remove(mediaTypeHeaderValue);
+            }
+
+            result.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            result.SerializerSettings.Converters.Add(new StringEnumConverter());
+            return result;
         }
     }
 }
