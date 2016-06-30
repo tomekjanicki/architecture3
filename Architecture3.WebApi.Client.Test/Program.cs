c18742242f7f223a70d51a6e7907d936392beba4
Tomasz Janicki <tomaszjanicki@ciber.com>
2016-06-30 14:41:06 +0200

    add client

---
+++ Architecture3.WebApi.Client.Test/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Architecture3.WebApi.Client.Test
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        private static void Main()
+        {
+            MainAsync().Wait();
+        }
+
+        private static async Task MainAsync()
+        {
+            try
+            {
+                var client = new Client(new Uri("http://localhost:2776/"));
+                var result = await client.ProductsFilterPaged(10, 5, string.Empty, string.Empty).ConfigureAwait(false);
+                Console.WriteLine("{0} {1}", result.Count, result.Items.Count);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
