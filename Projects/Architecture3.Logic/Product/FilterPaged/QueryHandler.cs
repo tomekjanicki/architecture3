9fb3b65c022a4c9f402e9ee1c670a5fd79488253
Tomasz Janicki <tomaszjanicki@ciber.com>
2016-07-01 09:34:28 +0200

    xxx

---
+++ Projects/Architecture3.Logic/Product/FilterPaged/QueryHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Architecture3.Logic.Product.FilterPaged
+{
+    using System.Collections.Generic;
+    using Architecture3.Common.Handlers.Interfaces;
+    using Architecture3.Common.ValueObjects;
+
+    public class QueryHandler : IRequestHandler<Query, Paged<Product>>
+    {
+        public Paged<Product> Handle(Query message)
+        {
+            // todo do sth
+            return Paged<Product>.Create(10, new List<Product>()).Value;
+        }
+    }
+}
