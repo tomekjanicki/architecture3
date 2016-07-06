0b7f9e36fd7389bf02087d5bfb7bf1a118041c9a
Tomasz Janicki <tomaszjanicki@ciber.com>
2016-07-06 09:33:51 +0200

    xxx

---
+++ Tests/Architecture3.Logic.Tests/CQ/Product/Delete/CommandHandlerTests.cs
@@ -0,0 +1,56 @@
+﻿namespace Architecture3.Logic.Tests.CQ.Product.Delete
+{
+    using Architecture3.Logic.CQ.Product.Delete;
+    using Architecture3.Logic.CQ.Product.Delete.Interfaces;
+    using Architecture3.Logic.Shared;
+    using Architecture3.Types;
+    using Architecture3.Types.FunctionalExtensions;
+    using NSubstitute;
+    using NUnit.Framework;
+    using Shouldly;
+
+    public class CommandHandlerTests
+    {
+        private CommandHandler _commandHandler;
+        private IRepository _repository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _repository = Substitute.For<IRepository>();
+            _commandHandler = new CommandHandler(_repository);
+        }
+
+        [Test]
+        public void ValidIdAndVersion_ShouldBeSuccess()
+        {
+            var command = GetValidCommand();
+
+            _repository.ExistsById(Arg.Any<NonNegativeInt>()).Returns(true);
+            _repository.GetRowVersionById(Arg.Any<NonNegativeInt>()).Returns(command.IdVersion.Version);
+            _repository.CanBeDeleted(Arg.Any<NonNegativeInt>()).Returns(true);
+
+            var result = _commandHandler.Handle(command);
+
+            result.IsSuccess.ShouldBeTrue();
+        }
+
+        [Test]
+        public void InvalidId_ShouldBeFaliure()
+        {
+            var command = GetValidCommand();
+
+            var result = _commandHandler.Handle(command);
+
+            result.IsFailure.ShouldBeTrue();
+            result.Error.ErrorType.ShouldBe(ErrorType.NotFound);
+        }
+
+        private static Command GetValidCommand()
+        {
+            var commandResult = Command.Create(1, "x");
+            commandResult.EnsureIsNotFaliure();
+            return commandResult.Value;
+        }
+    }
+}
---
+++ Tests/Architecture3.Logic.Tests/CQ/Product/Delete/CommandTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Architecture3.Logic.Tests.CQ.Product.Delete
+{
+    using Architecture3.Logic.CQ.Product.Delete;
+    using NUnit.Framework;
+    using Shouldly;
+
+    public class CommandTests
+    {
+        [Test]
+        public void ValidParameters_ShouldPass()
+        {
+            var commandResult = Command.Create(1, "X");
+            commandResult.IsSuccess.ShouldBeTrue();
+        }
+
+        [Test]
+        public void InvalidId_ShouldFail()
+        {
+            var commandResult = Command.Create(-1, "X");
+            commandResult.IsFailure.ShouldBeTrue();
+        }
+
+        [Test]
+        public void InvalidVersion_ShouldFail()
+        {
+            var commandResult = Command.Create(1, string.Empty);
+            commandResult.IsFailure.ShouldBeTrue();
+        }
+    }
+}
