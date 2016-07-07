ce19e67c78240b6ddf77130e2f14ada8c5232f87
Tomasz Janicki <tomaszjanicki@ciber.com>
2016-07-07 09:15:35 +0200

    xxx

--- Tests/Architecture3.Logic.Tests/CQ/Product/Delete/CommandTests.cs
+++ Tests/Architecture3.Logic.Tests/CQ/Product/Delete/CommandTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace Architecture3.Logic.Tests.CQ.Product.Delete
 {
     using Architecture3.Logic.CQ.Product.Delete;
+    using Architecture3.Types.FunctionalExtensions;
     using NUnit.Framework;
     using Shouldly;
 
@@ -25,6 +26,18 @@
         {
             var commandResult = Command.Create(1, string.Empty);
             commandResult.IsFailure.ShouldBeTrue();
+        }
+
+        [Test]
+        public void TwoCommands_ShouldBeEqual()
+        {
+            var commandResult1 = Command.Create(1, "X");
+            var commandResult2 = Command.Create(1, "X");
+
+            commandResult1.EnsureIsNotFaliure();
+            commandResult2.EnsureIsNotFaliure();
+
+            commandResult1.Value.ShouldBe(commandResult2.Value);
         }
     }
 }
