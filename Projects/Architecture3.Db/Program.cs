514d42d8164bb81e243e785b488fe8c5f4f4b5ae
Tomasz Janicki <tomaszjanicki@ciber.com>
2016-10-18 08:26:56 +0200

    add dbup

---
+++ Projects/Architecture3.Db/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Architecture3.Db
+{
+    using System;
+    using System.Configuration;
+    using System.Reflection;
+    using DbUp;
+    using log4net;
+
+    internal static class Program
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
+
+        private static int Main()
+        {
+            try
+            {
+                var connectionString = ConfigurationManager.ConnectionStrings["Main"].ConnectionString;
+
+                var upgrader = DeployChanges.To.SqlDatabase(connectionString).WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly()).LogToConsole().Build();
+
+                var result = upgrader.PerformUpgrade();
+
+                if (!result.Successful)
+                {
+                    Logger.Error(result.Error);
+                    DispalyMessage(ConsoleColor.Red, result.Error.ToString());
+                    return -1;
+                }
+
+                DispalyMessage(ConsoleColor.Green, "Success");
+                Logger.Info("Success");
+                return 0;
+            }
+            catch (Exception exception)
+            {
+                Logger.Error("Unhadled exception", exception);
+                return -2;
+            }
+        }
+
+        private static void DispalyMessage(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
