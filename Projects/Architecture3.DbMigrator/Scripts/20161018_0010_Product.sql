b8b9dda9defee31eaa1c78eb7237c17a1e139b13
Tomasz Janicki <tomaszjanicki@ciber.com>
2016-10-18 12:51:09 +0200

    add db project

---
+++ Projects/Architecture3.DbMigrator/Scripts/20161018_0010_Product.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Code] NVARCHAR(50) NOT NULL, 
+    [Name] NVARCHAR(100) NOT NULL, 
+    [Price] DECIMAL NOT NULL, 
+    [Version] ROWVERSION NOT NULL 
+)
+
+GO
+
+CREATE UNIQUE INDEX [IX_Product_Name] ON [dbo].[Product] ([Name])
