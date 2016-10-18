f994fde0439b5297db5441d673e77c4818ab756a
Tomasz Janicki <tomaszjanicki@ciber.com>
2016-10-18 11:22:13 +0200

    modification in build script

--- build_definition/buildDefinitionD.cmd
+++ build_definition/buildDefinitionD.cmd
@@ -1,2 +1,4 @@
 @echo off
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" .\buildDefinition.msbuild /nologo /m /v:m /t:D
+set /p OctopusNugetApiKey=Set Octopus Nuget api key
+set /p OctopusNugetServerUrl=Set Octopus Nuget server url
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" .\buildDefinition.msbuild /nologo /m /v:m /t:D /p:OctopusNugetApiKey=%OctopusNugetApiKey% /p:OctopusNugetServerUrl=%OctopusNugetServerUrl%
---
+++ build_definition/buildDefinitionDTest.cmd
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" .\buildDefinition.msbuild /nologo /m /v:m /t:DTest
